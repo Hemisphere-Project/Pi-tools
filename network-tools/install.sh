af6db50a0a048d6e858e01c93ea1f9ea8cc9064f
maigre <thomas@37m.gr>
2024-09-02 13:24:03 +0200

    webconf pipenv & wint

--- network-tools/install.sh
+++ network-tools/install.sh
@@ -3,16 +3,16 @@ BASEPATH="$(dirname "$(readlink -f "$0")")"
 
 cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 echo 'listen-address=10.0.0.1                             
-dhcp-range=10.0.0.2,10.0.0.99,255.255.255.0,12h
+dhcp-range=10.0.1.1,10.0.2.250,255.255.0.0,12h
+dhcp-leasefile=/var/lib/dnsmasq/dnsmasq.leases
 
-listen-address=10.1.0.1
+listen-address=10.1.0.1                             
 dhcp-range=10.1.0.2,10.1.0.99,255.255.255.0,12h
-
 dhcp-leasefile=/var/lib/dnsmasq/dnsmasq.leases
 ' > /etc/dnsmasq.conf
 
 mkdir -p /boot/wifi
-cp -r "$BASEPATH"/profiles/* /boot/wifi/
+#cp -r "$BASEPATH"/profiles/* /boot/wifi/
 
 ln -sf "$BASEPATH/setnet.service" /etc/systemd/system/
 ln -sf "$BASEPATH/setnet" /usr/local/bin/
@@ -26,7 +26,8 @@ ln -sf "$BASEPATH/uplink-fwd" /usr/local/bin/
 ln -sf "$BASEPATH/enforce-ping@.service" /etc/systemd/system/
 ln -sf "$BASEPATH/enforce-ping" /usr/local/bin/
 
-ln -sf "$BASEPATH/iface-off@.service" /etc/systemd/system/
+ln -sf "$BASEPATH/wlan0-off@.service" /etc/systemd/system/
+ln -sf "$BASEPATH/wint-off@.service" /etc/systemd/system/
 
 systemctl daemon-reload
 
@@ -34,10 +35,12 @@ FILE=/boot/starter.txt
 if test -f "$FILE"; then
 echo "## [network-tools] various network utilities
 # setnet                        # set NetworkManager profile from /boot/wifi
-# uplink-fwd@wlan0              # set interface as uplink and forward to other interfaces
+# uplink-fwd@wlan1              # set interface as uplink and forward to other interfaces
 # enforce-ping@2.0.0.1/wlan0    # ping <test-ip>, restart <iface> if failed
 # enforce-ipv4@eth0             # check that ipv4 has been properly set (usefull with late-join antenna)
-# iface-off@60/wlan0            # turn-off interface after <min> minutes
+# wlan0-off@60                  # turn-off wlan0 after <min> minutes
+# wint-off@60                  # turn-off wint after <min> minutes
 " >> /boot/starter.txt
 fi
+
 
--- webconf/flaskserver.py
+++ webconf/flaskserver.py
@@ -8,6 +8,10 @@ import netifaces as ni
 import eventlet, os, fileinput, sys, copy
 eventlet.monkey_patch()
 
+#
+# Sync Interface
+#
+syncIface = 0
 
 #
 # Utils
@@ -65,6 +69,67 @@ def uncommentline(find, file):
     except:
         pass
 
+def synciface(iface):
+    global syncIface
+    syncIface = int(iface)
+
+def syncgetiface():
+    os.system('sync')
+    return 0 if os.path.isfile('/boot/wifi/eth0-sync-STA.nmconnection') or os.path.isfile('/boot/wifi/eth0-sync-AP.nmconnection') else 1
+
+def syncmode(mode):
+    mode = int(mode)
+    os.system('rm /boot/wifi/wlan0-*')
+    os.system('rm /boot/wifi/eth0-*')
+    os.system('sync')
+    global syncIface
+    # ETH0
+    if syncIface == 0:
+        if mode == 1: os.system('cp /boot/wifi/_disabled/eth0-sync-STA.nmconnection /boot/wifi/eth0-sync-STA.nmconnection')
+        elif mode == 2: os.system('cp /boot/wifi/_disabled/eth0-sync-AP.nmconnection /boot/wifi/eth0-sync-AP.nmconnection')
+        else: os.system('cp /boot/wifi/_disabled/eth0-dhcp.nmconnection /boot/wifi/eth0-dhcp.nmconnection')
+    # WLAN0
+    elif syncIface == 1:
+        if mode == 1: os.system('cp /boot/wifi/_disabled/wlan0-sync-STA.nmconnection /boot/wifi/wlan0-sync-STA.nmconnection')
+        elif mode == 2: os.system('cp /boot/wifi/_disabled/wlan0-sync-AP.nmconnection /boot/wifi/wlan0-sync-AP.nmconnection')
+        os.system('cp /boot/wifi/_disabled/eth0-dhcp.nmconnection /boot/wifi/eth0-dhcp.nmconnection')
+    os.system('sync')
+
+
+def syncgetmode():
+    os.system('sync')
+    if os.path.isfile('/boot/wifi/wlan0-sync-AP.nmconnection') or os.path.isfile('/boot/wifi/eth0-sync-AP.nmconnection'):
+        return 2
+    elif os.path.isfile('/boot/wifi/wlan0-sync-STA.nmconnection') or os.path.isfile('/boot/wifi/eth0-sync-STA.nmconnection'):
+        return 1
+    else:
+        return 0
+
+def syncchannel(channel):
+    os.system('sync')
+    for file in ['/boot/wifi/wlan0-sync-AP.nmconnection',
+                 '/boot/wifi/wlan0-sync-STA.nmconnection',
+                 '/boot/wifi/_disabled/wlan0-sync-AP.nmconnection',
+                 '/boot/wifi/_disabled/wlan0-sync-STA.nmconnection']:
+        if os.path.isfile(file):
+            replaceline('ssid=', 'ssid=synclink-'+channel, file)
+    
+def syncgetchannel():
+    os.system('sync')
+    ssid = ''
+    file = None
+    for file in ['/boot/wifi/wlan0-sync-AP.nmconnection', 
+                 '/boot/wifi/wlan0-sync-STA.nmconnection', 
+                 '/boot/wifi/_disabled/wlan0-sync-AP.nmconnection',
+                 '/boot/wifi/_disabled/wlan0-sync-STA.nmconnection']:
+        if os.path.isfile(file):
+            break
+    if file:
+        ssid = getline('ssid=', file).split('=')[-1].split('#')[0].strip()
+    return ssid.split('-')[-1]
+
+
+
 #
 # Threaded Flask Server
 #
@@ -74,39 +139,66 @@ class FlaskServer():
         self._settings = {}
         
         self._settings['hostname'] = {
-                'label':    'Hostname',
+                'label':    'Name',
                 'field':    'text|15',
-                'legend':   '',
+                'legend':   '<br /><br />',
                 'value':    getline('hostrename@', '/boot/starter.txt').split('@')[-1].split('#')[0].strip(),
                 'apply':    lambda value: replaceline('hostrename@', 'hostrename@'+value.strip(), '/boot/starter.txt') if value.strip() != '' else False
             }
         
-        if os.path.isfile('/boot/wifi/wlan0-hotspot.nmconnection'):
-            self._settings['hostname']['label'] = 'Hotspot'
+        if os.path.isfile('/boot/wifi/wint-hotspot.nmconnection'):
+            # self._settings['hostname']['label'] = 'Hotspot'
             self._settings['wifipass'] = {
-                    'label':    'Hotspot password',
+                    'label':    'Admin-wifi password',
                     'field':    'text|15',
                     'legend':   '8 char. minimum',
-                    'value':     getline('psk=', '/boot/wifi/wlan0-hotspot.nmconnection').split('=')[-1].split('#')[0].strip(),
-                    'apply':     lambda value: replaceline('psk=', 'psk='+value.strip(), '/boot/wifi/wlan0-hotspot.nmconnection') if len(value.strip()) >= 8 else False
+                    'value':     getline('psk=', '/boot/wifi/wint-hotspot.nmconnection').split('=')[-1].split('#')[0].strip(),
+                    'apply':     lambda value: replaceline('psk=', 'psk='+value.strip(), '/boot/wifi/wint-hotspot.nmconnection') if len(value.strip()) >= 8 else False
                 }
 
-        if os.path.isfile('/boot/wifi/wlan0-hotspot.nmconnection') or getline('wlan0-off@', '/boot/starter.txt')[0] != '#':
+        if os.path.isfile('/boot/wifi/wint-hotspot.nmconnection') or getline('wint-off@', '/boot/starter.txt')[0] != '#':
             self._settings['wlan-off'] = {
-                    'label':    'Wifi OFF',
+                    'label':    'Admin-wifi OFF',
                     'field':    'text|15',
-                    'legend':   'seconds (0 to disable WIFI-OFF)',
-                    'value':     getline('wlan0-off@', '/boot/starter.txt').split('@')[-1].split('#')[0].strip() if getline('wlan0-off@', '/boot/starter.txt')[0] != '#' else '0',
-                    'apply':     lambda value: replaceline('wlan0-off@', 'wlan0-off@'+str(int(value)), '/boot/starter.txt') if int(value) > 0 else replaceline('wlan0-off@', '# wlan0-off@'+str(int(value)), '/boot/starter.txt')
+                    'legend':   'seconds (0 to disable WIFI-OFF)<br /><br />',
+                    'value':     getline('wint-off@', '/boot/starter.txt').split('@')[-1].split('#')[0].strip() if getline('wint-off@', '/boot/starter.txt')[0] != '#' else '0',
+                    'apply':     lambda value: replaceline('wint-off@', 'wint-off@'+str(int(value)), '/boot/starter.txt') if int(value) > 0 else replaceline('wint-off@', '# wint-off@'+str(int(value)), '/boot/starter.txt')
                 }
-
-        self._settings['hdmi'] = {
-                'label':    'HDMI mode',
-                'field':    'text|15',
-                'legend':   '82: 1080p / 85: 720p / 16: 1024x768 / 51: 1600x1200 / 9: 800x600',
-                'value':     getline('hdmi_mode=', '/boot/config.txt').split('=')[-1].split('#')[0].strip(),
-                'apply':     lambda value: replaceline('hdmi_mode=', 'hdmi_mode='+str(int(value)), '/boot/config.txt') if int(value) > 0 else False
+		
+        if os.path.isfile('/boot/config.txt'): 
+            self._settings['hdmi'] = {
+                    'label':    'HDMI mode',
+                    'field':    'select|1080p[82],720p[85],1600x1200[51],1366x768[81],1024x768[16],800x600[9]',
+                    'legend':   '<br /><br />',
+                    'value':     getline('hdmi_mode=', '/boot/config.txt').split('=')[-1].split('#')[0].strip(),
+                    'apply':     lambda value: replaceline('hdmi_mode=', 'hdmi_mode='+str(int(value)), '/boot/config.txt') if int(value) > 0 else False
+                }
+        
+        self._settings['synciface'] = {
+                'label':    'SYNC interface',
+                'field':    'select|eth0[0],wlan0[1]',
+                'legend':   'eth0: wired / wlan0: wifi dongle',
+                'value':     syncgetiface(),
+                'apply':     lambda value: synciface(value)
+                
+        }
+        
+        self._settings['syncmode'] = {
+                'label':    'SYNC mode',
+                'field':    'select|disable[0],slave[1],master[2]',
+                'legend':   '',
+                'value':     syncgetmode(),
+                'apply':     lambda value: syncmode(value)
             }
+        
+        self._settings['syncchannel'] = {
+                'label':    'SYNC channel',
+                'field':    'text|3',
+                'legend':   'channel alias for Wifi Sync (allows distinct sync network, wifi only)<br /><br />',
+                'value':     syncgetchannel(),
+                'apply':     lambda value: syncchannel(value)
+            }
+        
         
         # for line in fileinput.input('/boot/starter.txt'):
         #     if not line.startswith('#'):
@@ -187,7 +279,9 @@ class FlaskServer():
                 if key in self._settings:
                     self._settings[key]['apply'](value)
             os.system('sync')
+            os.system('setnet')
             os.system('ro')
+            os.system('nmcli c d eth0')
             self.refresh()
             socketio.emit('settings', self.settings())
             print('updated: ', values)
